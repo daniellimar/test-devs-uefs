53e43f0e0ceae319257942d4dc8c23a4d134cdd0
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2025-11-11 17:21:23 -0400

    Feature Create config/corporate

---
+++ api/config/corporate.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Domínio Corporativo
+    |--------------------------------------------------------------------------
+    | Domínio de e-mail permitido para usuários da empresa
+    */
+    'domain' => env('CORPORATE_DOMAIN', 'postflow.com'),
+];
